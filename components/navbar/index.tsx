c8c10320d66b155a7d16878d116cd9604d03f5d8
tuppers360 <tuppers360@live.co.uk>
2020-09-28 07:36:55 +0100

    add navbar structure

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import Link from "next/link";
+import { MdFingerprint } from "react-icons/md";
+import { FaBars, FaTimes } from "react-icons/fa";
 import styles from "./navbar.module.scss";
 
 function NavBar() {
@@ -11,12 +13,34 @@ function NavBar() {
     <>
       <div className="navbar">
         <div className="navbar-container container">
-          <Link href="/" className="navbar-logo">
-            <a>Lavish</a>
+          <Link href="/">
+            <a className="navbar-logo">
+              <MdFingerprint className="navbar-icon" />
+              Lavish
+            </a>
           </Link>
         </div>
+        <div className="menu-icon" onClick={handleClick}>
+          {click ? <FaTimes /> : <FaBars />}
+        </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link href="/">
+              <a className="nav-links">Home</a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/stories">
+              <a className="nav-links">Stories</a>
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link href="/about">
+              <a className="nav-links">About</a>
+            </Link>
+          </li>
+        </ul>
       </div>
-      <div className={styles.navBarStyle}>NAVBAR</div>
     </>
   );
 }
